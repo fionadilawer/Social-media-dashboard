918fc198ab88d18bb3737fa2119698e3419d6639
KanzariAmine <kanzari.amin@gmail.com>
2020-04-15 22:17:12 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let checkbox = document.querySelector("input[name=theme]");
+
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    trans();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    trans();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+let trans = () => {
+  console.log(document.documentElement);
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
